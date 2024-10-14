2e4da36696bff1fd44fb09c1d0601b8e35e39ca6
Sebastian Mosquera <sebimosquera@gmail.com>
2024-10-14 17:33:56 -0300

    fix typo

--- src/components/pages/Optimizacion.jsx
+++ src/components/pages/Optimizacion.jsx
@@ -138,7 +138,7 @@ export const Optimizacion = () => {
     <div className="row">
         <div className="col-sm-12 col-md-12 p-4 mt-3">
             <div className="d-flex align-items-center justify-content-start mb-2">
-                <h6>Características</h6>
+                <h6 className="mb-4">Esta optimización completa ofrece:</h6>
             </div>
 
             <div className="d-flex align-items-center justify-content-start mb-2">
